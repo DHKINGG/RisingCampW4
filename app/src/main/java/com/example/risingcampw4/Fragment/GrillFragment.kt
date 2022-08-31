055e5e53b84fe15cd82733fed0be01654add61c9
donghwikkim <donghwi9507@gmail.com>
2022-08-31 13:54:43 +0900

    붕어빵 타이쿤 구현중

--- app/src/main/java/com/example/risingcampw4/Fragment/GrillFragment.kt
+++ app/src/main/java/com/example/risingcampw4/Fragment/GrillFragment.kt
@@ -1,5 +1,6 @@
 package com.example.risingcampw4.Fragment
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import com.example.risingcampw4.Model.FishBread
@@ -11,11 +12,13 @@ import com.example.risingcampw4.R
 import com.example.risingcampw4.databinding.FragmentGrillBinding
 
 class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::inflate) {
-    private var grills = mutableListOf<FishBread>()
-    private var grillImageViews = mutableListOf<ImageView>()
-    private var redBeanImageViews = mutableListOf<ImageView>()
-    private var redBeanCreamImageViews = mutableListOf<ImageView>()
-    private var isRedBeanBottle = false
+    private var grills = mutableListOf<FishBread>() // 붕어빵 상태배열
+    private var grillImageViews = mutableListOf<ImageView>()    // 붕어빵 이미지뷰 배열
+    private var redBeanImageViews = mutableListOf<ImageView>()  // 붕어빵 눈 이미지뷰 배열
+    private var redBeanCreamImageViews = mutableListOf<ImageView>() // 붕어빵 위의 크림 이미뷰 배열
+    private var isRedBeanBottle = false // 팥 병 선택 여부
+    private var score = 0   // 점수
+    private var heart = 5   // 목숨 개수
 
     override fun initView() {
         super.initView()
@@ -76,14 +79,16 @@ class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::i
     private fun setOnClickListener() {
         for (i in 0..8) {
             grillImageViews[i].setOnClickListener {
-                if (isRedBeanBottle) {
-                    if (!grills[i].isTurnOver && grills[i].breadStatus > 0) {
+                if (isRedBeanBottle) {  // 팥 병 클릭됐을때
+                    if (!grills[i].isTurnOver && grills[i].breadStatus > EMPTY_GRILL) {
                         addCream(i)
                     }
+                } else if (grills[i].backBreadStatus > EMPTY_GRILL && grills[i].isTurnOver) { // 뒷면일때 클릭
+                    takeOutBread(i)
                 } else {
-                    if (grills[i].breadStatus > 0 && !grills[i].isTurnOver) {
+                    if (grills[i].breadStatus > EMPTY_GRILL && !grills[i].isTurnOver) { // 팥 병 활성화 안되고 앞면일때 클릭
                         backStartBake(i)
-                    } else {
+                    } else {    // 빈 그릴일때 클릭
                         startBake(i)
                     }
                 }
@@ -112,8 +117,9 @@ class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::i
                         activity?.runOnUiThread {
                             changeGrillStatus(index)
                         }
+                    } else {
+                        break
                     }
-
                 }
             } catch (e: InterruptedException) {
                 e.printStackTrace()
@@ -122,7 +128,7 @@ class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::i
     }
 
     private fun backStartBake(index: Int) {
-        grills[index].backBreadStatus = 1
+        grills[index].backBreadStatus = RARE
         grills[index].isTurnOver = true
         redBeanCreamImageViews[index].visibility = View.GONE
         changeBackGrillStatus(index)
@@ -136,6 +142,8 @@ class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::i
                         activity?.runOnUiThread {
                             changeBackGrillStatus(index)
                         }
+                    } else {
+                        break
                     }
                 }
             } catch (e: InterruptedException) {
@@ -175,11 +183,33 @@ class GrillFragment : BaseFragment<FragmentGrillBinding>(FragmentGrillBinding::i
     }
 
     private fun addCream(index: Int) {
-        if (!grills[index].isTurnOver && grills[index].breadStatus > 0) {
+        if (!grills[index].isTurnOver && grills[index].breadStatus > EMPTY_GRILL) {
             grills[index].isRedBean = true
             redBeanCreamImageViews[index].visibility = View.VISIBLE
             binding.ivRedBeanBottle.setBackgroundColor(resources.getColor(R.color.white))
             isRedBeanBottle = !isRedBeanBottle
         }
+    }
+
+    private fun takeOutBread(index: Int) {
+        if (grills[index].breadStatus == WELL_DONE && grills[index].backBreadStatus == WELL_DONE && grills[index].isRedBean) {
+            score += 10
+        } else {
+            heart -= 1
+        }
+
+        clearBread(index)
+        grillImageViews[index].setImageResource(R.drawable.mold)
+        redBeanCreamImageViews[index].visibility = View.GONE
+        redBeanImageViews[index].visibility = View.GONE
+
+        Log.d("myLog", "score = ${score}, heart = ${heart}")
+    }
+
+    private fun clearBread(index: Int) {
+        grills[index].breadStatus = 0
+        grills[index].backBreadStatus = 0
+        grills[index].isRedBean = false
+        grills[index].isTurnOver = false
     }
 }
